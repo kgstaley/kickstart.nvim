bb7518cd77dff845153486dfa14e6daea68a8279
Kerry Staley <35789718+kgstaley@users.noreply.github.com>
2024-03-19 18:21:38 -0700

    add go to autoformat, enable custom plugins

--- init.lua
+++ init.lua
@@ -754,6 +754,7 @@ require('lazy').setup({
         javascript = { { 'prettierd', 'prettier' } },
         typescript = { { 'prettierd', 'prettier' } },
         ruby = { { 'rubocop' } },
+        go = { { 'gofmt' } },
       },
     },
   },
@@ -982,7 +983,7 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
