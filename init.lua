738eeeb6d0336287331e722d255173593d0e2b84
Kerry Staley <35789718+kgstaley@users.noreply.github.com>
2024-03-28 18:14:13 -0700

    set filetypes for ruby_ls and tsserver

--- init.lua
+++ init.lua
@@ -719,15 +719,19 @@ require('lazy').setup({
         gopls = {},
         pyright = {},
         rust_analyzer = {},
-        ruby_ls = {},
+        ruby_ls = {
+          filetypes = { 'ruby' },
+        },
         -- rubocop = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        -- But for many setups, the LSP (`tsserver`) will work just fine
+        tsserver = {
+          filetypes = { 'html', 'typescript', 'typescriptreact', 'javascript', 'javascriptreact', 'typescript.tsx' },
+        },
         --
 
         lua_ls = {
