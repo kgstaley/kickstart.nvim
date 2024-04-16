dc52eef762bc9a0546b4594c3fe40d8345d7e75b
Kerry Staley <35789718+kgstaley@users.noreply.github.com>
2024-04-16 10:17:22 -0700

    update ruby lsp

--- init.lua
+++ init.lua
@@ -717,7 +717,7 @@ require('lazy').setup({
         gopls = {},
         pyright = {},
         rust_analyzer = {},
-        ruby_ls = {
+        ruby_lsp = {
           filetypes = { 'ruby' },
         },
         -- rubocop = {},
