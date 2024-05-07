66a6019aeb8828afd98e8f9d1ad5839bacce8e8e
Kerry Staley <35789718+kgstaley@users.noreply.github.com>
2024-05-07 13:13:35 -0700

    integrate main https://github.com/nvim-lua/kickstart.nvim/pull/900 to lspconfig file

--- lua/kickstart/plugins/nvim-lspconfig.lua
+++ lua/kickstart/plugins/nvim-lspconfig.lua
@@ -118,6 +118,14 @@ return { -- LSP Configuration & Plugins
             group = highlight_augroup,
             callback = vim.lsp.buf.clear_references,
           })
+
+            vim.api.nvim_create_autocmd('LspDetach', {
+              group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+              callback = function(event2)
+                vim.lsp.buf.clear_references()
+                vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+              end,
+            })
         end
 
         -- The following autocommand is used to enable inlay hints in your
