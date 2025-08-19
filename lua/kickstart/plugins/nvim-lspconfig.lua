a5e2f18726926eb06e810aeb5f54d930bb208fe4
Kerry Staley <35789718+kgstaley@users.noreply.github.com>
2025-08-19 12:06:08 -0700

    readd language lsps

--- lua/kickstart/plugins/nvim-lspconfig.lua
+++ lua/kickstart/plugins/nvim-lspconfig.lua
@@ -210,9 +210,9 @@ return   -- LSP Plugins
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        gopls = {},
-        pyright = {},
-        rust_analyzer = {},
+        gopls = { 'go' },
+        pyright = { 'py' },
+        rust_analyzer = { 'rust' },
         solargraph = {
           filetypes = { 'ruby' },
         },
